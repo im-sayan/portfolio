2f83ea88242dc29ea73aff3d32f6ead70b680762
sayan <heysayan192@gmail.com>
2024-03-03 00:12:02 +0530

    modification

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyB-Yem4VbCe8Gkmj-jtcUZmvsYUfd9VoXw",
+    authDomain: "portfolio-4911e.firebaseapp.com",
+    projectId: "portfolio-4911e",
+    storageBucket: "portfolio-4911e.appspot.com",
+    messagingSenderId: "202245580050",
+    appId: "1:202245580050:web:17e2e4fa34abc7b63d7fcb",
+    measurementId: "G-C1JCNMMWST"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+firebaseConfig : {
+  apiKey: "AIzaSyB-Yem4VbCe8Gkmj-jtcUZmvsYUfd9VoXw",
+  authDomain: "portfolio-4911e.firebaseapp.com",
+  projectId: "portfolio-4911e",
+  storageBucket: "portfolio-4911e.appspot.com",
+  messagingSenderId: "202245580050",
+  appId: "1:202245580050:web:17e2e4fa34abc7b63d7fcb",
+  measurementId: "G-C1JCNMMWST"
+}
 };
 
 /*
